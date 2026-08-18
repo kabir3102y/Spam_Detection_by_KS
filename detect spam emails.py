eab80d18c4f9546df6a5fe4751693d55018e0d41
Kabir Singh <kabirs3102@gmail.com>
2026-08-18 14:03:34 +0530

    Add files via upload
    Spam Email Detection is a machine learning project that automatically identifies whether an email or message is spam or not spam (ham).
    
    Spam messages are unwanted messages that may contain advertisements, scams, misleading information, or unnecessary content. Manually checking every message is difficult, so machine learning can be used to automatically classify them.
    
    The system learns patterns from previously labeled messages and uses those patterns to predict the category of a new message.

---
+++ detect spam emails.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix
+
+df = pd.read_csv(r"C:\Users\Amarjeet Singh\Downloads\spam.csv", encoding="latin-1")
+df = df.iloc[:, :2]
+df.columns = ["label", "message"]
+df["label"] = df["label"].map({"ham": 0, "spam": 1})
+
+X = df["message"]
+y = df["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+vectorizer = CountVectorizer()
+
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
+
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+
+TN = cm[0][0]
+FP = cm[0][1]
+FN = cm[1][0]
+TP = cm[1][1]
+
+accuracy = (TP + TN) / (TP + TN + FP + FN)
+precision = TP / (TP + FP)
+recall = TP / (TP + FN)
+f1_score = 2 * (precision * recall) / (precision + recall)
+
+print("True Positive :", TP)
+print("True Negative :", TN)
+print("False Positive:", FP)
+print("False Negative:", FN)
+
+print("\nAccuracy :", round(accuracy * 100, 2), "%")
+print("Precision:", round(precision * 100, 2), "%")
+print("Recall   :", round(recall * 100, 2), "%")
+print("F1 Score :", round(f1_score * 100, 2), "%")
+
+while True:
+    msg = input("\nEnter Email (or type exit): ")
+
+    if msg.lower() == "exit":
+        break
+
+    msg_vector = vectorizer.transform([msg])
+
+    prediction = model.predict(msg_vector)
+
+    if prediction[0] == 1:
+        print("Spam Mail")
+    else:
+        print("Not Spam Mail")
